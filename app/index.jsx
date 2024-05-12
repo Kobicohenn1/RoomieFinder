e7d2695ce00233a17a4dbf403a8cb7f61f8279eb
kobicohen <kobicohenv1.1@gmail.com>
2024-05-12 17:43:41 +0300

    setup navigation add custom_fonts

---
+++ app/index.jsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <ScrollView contentContainerStyle={{ height: '100%' }}></ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontFamily: 'Poppins-Black',
+    fontSize: 24,
+  },
+});
